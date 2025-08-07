0d6bb82bf9540696684cb807955b032e2c2673cf
guilhermemm9 <130560970+guilhermemm9@users.noreply.github.com>
2025-08-07 10:41:52 -0300

    Caminho corrigido

--- Ferramenta/lista_fundos_analisados.py
+++ Ferramenta/lista_fundos_analisados.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Caminho do arquivo
 #arquivo = r"C:\Users\User\Documents\OneDrive\Documentos\Guilherme\Precos_Reuters.xlsm"
-arquivo = r"C:\Users\User\Documents\OneDrive\Documentos\Guilherme\Códigos\Longview_FIIs\acompanhamento-fiis\Precos_Reuters.xlsm"
+arquivo = r"./Preços_Reuters.xlsm"
 # Abre apenas a aba "Preços"
 df_precos = pd.read_excel(arquivo, sheet_name="Preços", header=None)
 
